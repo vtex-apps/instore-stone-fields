c012e7cf46ac7cd8e7a38252263538986b25d067
Guilherme Bruzzi <guibruzzi@gmail.com>
2018-10-03 13:16:43 -0300

    Remove commments

--- react/index.js
+++ react/index.js
@@ -4,13 +4,6 @@ import FormInput from 'vtex.instore-form-fields/FormInput'
 class StoneFields extends React.Component {
   render() {
     return null
-    // return "" (
-    //   <React.Fragment>
-    //     <FormInput ffor="stonecode" nameKey="instore-stone-fields.stone-code"/>
-    //     <FormInput ffor="mothersname" nameKey="instore-stone-fields.mothers-name"/>
-    //     <FormInput ffor="businessname" nameKey="instore-stone-fields.business-name"/>
-    //   </React.Fragment>
-    // )
   }
 }
 
