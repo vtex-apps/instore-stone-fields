b93a6379cb6767d28509e7379b6156832039e93e
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-02-19 17:24:49 -0300

    Migrate to new pages using plugins

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- Upgrade react builder to v3, pages to v1, and messages to v1.
 
 ## [0.1.5] - 2019-01-14
 
--- react/index.js
+++ react/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import FormInput from 'vtex.instore-form-fields/FormInput'
 
-class StoneFields extends React.Component {
-  render() {
-    return null
-  }
+const StoneFields = () => {
+  return (
+    <Fragment>
+      <FormInput
+        ffor="customsenha"
+        nameKey="instore-stone-fields.stone-code"
+        isRequired
+      />
+      <FormInput
+        ffor="customnomemae"
+        nameKey="instore-stone-fields.mothers-name"
+        isRequired={false}
+      />
+      <FormInput
+        ffor="nomefantasiapessoal"
+        nameKey="instore-stone-fields.business-name"
+        isRequired={false}
+      />
+    </Fragment>
+  )
 }
 
 export default StoneFields
